54f7dd0a8fb607d79636d47928aab9703df5f151
g-Lorkas <giorgoslordopoulos@gmail.com>
2017-10-31 20:29:47 +0200

    Created: 1)on MainActivity: buttons for business/user ports 2)on BusinessActivity: buttons for register/login 3)on UserActivity: buttons for register/login 4)activities for register/login

--- app/src/main/java/com/example/lord/goldenoffers/user/LoginActivity.java
+++ app/src/main/java/com/example/lord/goldenoffers/user/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.example.lord.goldenoffers.user;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import com.example.lord.goldenoffers.R;
 
@@ -10,6 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_login);
+        setContentView(R.layout.activity_user_login);
     }
 }
