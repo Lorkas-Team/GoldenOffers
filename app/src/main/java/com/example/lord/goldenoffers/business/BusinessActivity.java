b0ef6ad1e51cb2b19ae0151a4d907ae8c2cfce11
g-Lorkas <giorgoslordopoulos@gmail.com>
2017-10-31 23:08:35 +0200

    Created: 1)on MainActivity: buttons for business/user ports 2)on BusinessActivity: buttons for register/login 3)on UserActivity: buttons for register/login 4)activities for register/login

--- app/src/main/java/com/example/lord/goldenoffers/business/BusinessActivity.java
+++ app/src/main/java/com/example/lord/goldenoffers/business/BusinessActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import com.example.lord.goldenoffers.R;
 
 public class BusinessActivity extends AppCompatActivity {
-    //lord branch123
+    //lord branch1234
     //test
     public Button loginButton;
     public Button registerButton;
