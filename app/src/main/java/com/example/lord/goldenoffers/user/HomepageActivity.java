64a82946545c1d93df1d0de8952eaf86613d4e14
IoanDragk <gimmebeta@outlook.com>
2017-12-06 00:59:05 +0200

    Added sessiong manager logoutbutton and the rest in homepageactivity.

--- app/src/main/java/com/example/lord/goldenoffers/user/HomepageActivity.java
+++ app/src/main/java/com/example/lord/goldenoffers/user/HomepageActivity.java
@@ -7,8 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.lord.goldenoffers.R;
+import com.example.lord.goldenoffers.helper.SQLiteHandlerForUsers;
+import com.example.lord.goldenoffers.helper.SessionManager;
 
 public class HomepageActivity extends AppCompatActivity {
+
+    private SQLiteHandlerForUsers db;
+    private SessionManager session;
 
     public Button AddWishButton;
     public Button ViewWishButton;
@@ -50,12 +55,48 @@ public class HomepageActivity extends AppCompatActivity {
             }
         });
 
-        //Still needs a logoutButton
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+
+        // SqLite database handler
+        db = new SQLiteHandlerForUsers(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Logout button click event
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(UserLoggedInActivity.this, UserLoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
