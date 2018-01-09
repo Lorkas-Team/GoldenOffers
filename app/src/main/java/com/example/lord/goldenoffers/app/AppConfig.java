47fee724ded4aa48295f54b3aa1f4352ab88043c
g-Lorkas <giorgoslordopoulos@gmail.com>
2018-01-09 17:37:34 +0200

    fix names of links for php files

--- app/src/main/java/com/example/lord/goldenoffers/app/AppConfig.java
+++ app/src/main/java/com/example/lord/goldenoffers/app/AppConfig.java
@@ -2,28 +2,28 @@ package com.example.lord.goldenoffers.app;
 
 public class AppConfig {
 
-    public static String URL_LOGIN = "https://golden-offers.000webhostapp.com/API/Business_Register.Login/Login.php";
+    public static String URL_LOGIN = "https://golden-offers.000webhostapp.com/API/Business/Login.php";
 
-    public static String URL_REGISTER = "https://golden-offers.000webhostapp.com/API/Business_Register.Login/Register.php";
+    public static String URL_REGISTER = "https://golden-offers.000webhostapp.com/API/Business/Register.php";
 
 
-    public static String USER_URL_REGISTER = "https://golden-offers.000webhostapp.com/API/Users_API_by_george/Register.php";
+    public static String USER_URL_REGISTER = "https://golden-offers.000webhostapp.com/API/Users/Register.php";
 
-    public static String USER_URL_LOGIN = "https://golden-offers.000webhostapp.com/API/Users_API_by_george/Login.php";
+    public static String USER_URL_LOGIN = "https://golden-offers.000webhostapp.com/API/Users/Login.php";
 
-    public static String URL_ADD_OFFER = "https://golden-offers.000webhostapp.com/API/Business_Register.Login/UploadOffer.php";
+    public static String URL_ADD_OFFER = "https://golden-offers.000webhostapp.com/API/Business/UploadOffer.php";
 
-    public static String URL_MY_OFFERS = "https://golden-offers.000webhostapp.com/API/Business_Register.Login/DisplayOffers.php";
+    public static String URL_MY_OFFERS = "https://golden-offers.000webhostapp.com/API/Business/DisplayOffers.php";
 
-    public static String USER_URL_ADD_DESIRE = "https://golden-offers.000webhostapp.com/API/Users_API_by_george/Add_New_Desire.php";
+    public static String USER_URL_ADD_DESIRE = "https://golden-offers.000webhostapp.com/API/Users/Add_New_Desire.php";
 
-    public static String USER_URL_GET_DESIRES ="https://golden-offers.000webhostapp.com/API/Users_API_by_george/Get_Users_Desires.php";
+    public static String USER_URL_GET_DESIRES ="https://golden-offers.000webhostapp.com/API/Users/Get_Users_Desires.php";
 
-    public static String USER_GET_OFFERS_FROM_DESIRES="https://golden-offers.000webhostapp.com/API/Users_API_by_george/Get_Offers_From_Desires.php";
+    public static String USER_GET_OFFERS_FROM_DESIRES="https://golden-offers.000webhostapp.com/API/Users/Get_Offers_From_Desires.php";
 
-    public static String USER_DESIRE_UPDATE="https://golden-offers.000webhostapp.com/API/Users_API_by_george/Update_Desire.php";
+    public static String USER_DESIRE_UPDATE="https://golden-offers.000webhostapp.com/API/Users/Update_Desire.php";
 
-    public static String OFFER_UPDATE="https://golden-offers.000webhostapp.com/API/Business_Register.Login/Update_Offers.php";
+    public static String OFFER_UPDATE="https://golden-offers.000webhostapp.com/API/Business/Update_Offers.php";
 
-    public static String OFFER_DELETE="https://golden-offers.000webhostapp.com/API/Business_Register.Login/Delete_Offers.php";
+    public static String OFFER_DELETE="https://golden-offers.000webhostapp.com/API/Business/Delete_Offers.php";
 }
